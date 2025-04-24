5d00985c7807fd2078b2f39a0fd0e311c4ae23d7
jazzvillainy <crescentdede@gmail.com>
2025-04-24 13:51:14 +0100

    bold title

--- todofr/src/components/NoteCards.jsx
+++ todofr/src/components/NoteCards.jsx
@@ -3,9 +3,11 @@ import React from "react";
 function NoteCards({ item }) {
   return (
     <div className="bg-white bg-opacity-5 h-fit py-5 px-2 w-[90%] rounded-lg flex flex-col text-white">
-      <span>{item.title}</span>
+      <h1 className="font-bold">{item.title}</h1>
       <span>
-        {item.content.length > 45 ? item.content.slice(0, 45) + "..." : item.content}
+        {item.content.length > 45
+          ? item.content.slice(0, 45) + "..."
+          : item.content}
       </span>
       <span className="pt-1 text-stone-500">
         {item.created_at.toLocaleString()}
